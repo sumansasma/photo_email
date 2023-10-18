2deec9d56f721585d741f8b18022e592d919a90c
Suman <145822623+sumansasma@users.noreply.github.com>
2023-10-18 15:02:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -27,17 +27,17 @@ app.post('/send_email', upload.single('image'), (req, res) => {
 
     // Create a Nodemailer transporter
     const transporter = nodemailer.createTransport({
-        service: 'YourEmailService', // e.g., 'Gmail'
+        service: 'gmail.com', // e.g., 'Gmail'
         auth: {
-            user: 'your-email@example.com', // Replace with your email
-            pass: 'your-password', // Replace with your email password
+            user: 'sijgeriaucssangha@gmail.com', // Replace with your email
+            pass: 'cukc drra ypkd viay', // Replace with your email password
         },
     });
 
     // Compose the email
     const mailOptions = {
         from: 'your-email@example.com',
-        to: 'recipient@example.com', // Replace with the recipient's email
+        to: 'sijgeriaucssangha@gmail.com', // Replace with the recipient's email
         subject: 'Contact Form Submission',
         text: `Name: ${name}\nEmail: ${email}\nDescription: ${description}`,
         attachments: [
@@ -47,21 +47,12 @@ app.post('/send_email', upload.single('image'), (req, res) => {
             },
         ],
     };
-    // Inside the email sending function in server.js
-transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-        console.error('Error:', error);
-        res.json({ success: false, error: error.message });
-    } else {
-        console.log('Email sent:', info.response);
-        res.json({ success: true });
-    }
-});
+
     // Send the email
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
             console.error('Error:', error);
-            res.json({ success: false, error: error.message }); // Return an error message
+            res.json({ success: false });
         } else {
             console.log('Email sent:', info.response);
             res.json({ success: true });
@@ -71,6 +62,7 @@ transporter.sendMail(mailOptions, (error, info) => {
         fs.unlinkSync(req.file.path);
     });
 });
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
